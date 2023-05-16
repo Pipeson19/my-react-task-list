45b8ee1c079efbcc394cdd49fe1875ec4e972565
Felipe Salazar <felipesalazar19@hotmail.com>
2023-05-15 22:04:34 -0500

    cambios para el proyecto 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import Header from './components/Header';
-import TaskList from './components/TaskList';
+import React, { useState, useEffect } from "react";
 
-const App = () => {
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
   return (
     <div>
-      <Header />
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add Task</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>My React Task List</h1>
       <TaskList />
     </div>
   );
-};
+}
 
 export default App;
